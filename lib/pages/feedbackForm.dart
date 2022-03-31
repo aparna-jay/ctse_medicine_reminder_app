78efd3c0da82fe96f4954e6da2553f5ab6cd02fb
gavin <gavingunapala06@gmail.com>
2022-03-31 12:39:43 +0530

    feedback minor changers

--- lib/pages/feedbackForm.dart
+++ lib/pages/feedbackForm.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 import 'home.dart';
 
@@ -20,49 +21,57 @@ class FeedbackForm extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Feedback" ),
       ),
-      body:Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children:[
-              const Text("Add your feedback" , style: TextStyle(fontWeight: FontWeight.bold, height: 2, fontSize: 25 , color: Colors.blue),),
-              const SizedBox(height: 3),
-              TextField(
-                controller: _nameController,
-                decoration: const InputDecoration(
-                    border:OutlineInputBorder(
-                        borderSide:BorderSide(color: Colors.limeAccent)
-                    ),
-                    labelText: ('Your Email address')
-                ),
-              ),
-              const SizedBox(height: 3),
-              Card(
-                  color: Colors.white70,
-
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextField(
-                      controller: _feedController,
-                      maxLines: 8,
-                      decoration: const InputDecoration.collapsed(hintText: "Enter your text here"),
-                    ),
-                  )
-              ),
-              Container(
-                margin: const EdgeInsets.all(25),
-                child: OutlinedButton(
-                  onPressed: (){
-                    clear();
-                    Navigator.of(context).pushNamed(Home.routeName);
-                  },
-                  child:
-                  const Text("Add FeedBack",
-                    style: TextStyle(fontSize: 20.0)
+      body:Center(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children:[
+                const Text("Add your feedback" , style: TextStyle(fontWeight: FontWeight.bold, height: 2, fontSize: 30 , color: Colors.blue),),
+                const SizedBox(height: 35),
+                TextField(
+                  controller: _nameController,
+                  decoration: const InputDecoration(
+                      border:OutlineInputBorder(
+                          borderSide:BorderSide(color: Colors.limeAccent)
+                      ),
+                      labelText: ('Your Email address')
                   ),
                 ),
-              )
-            ],
+                const SizedBox(height: 10),
+                Card(
+                    color: Colors.white70,
+
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                        controller: _feedController,
+                        maxLines: 8,
+                        decoration: const InputDecoration.collapsed(hintText: "Enter your text here"),
+                      ),
+                    )
+                ),
+                const SizedBox(height: 10),
+                RatingBar.builder(
+                  minRating:1,
+                  itemBuilder:(context , _) => const Icon(Icons.star , color: Colors.amberAccent)
+                  , onRatingUpdate: (double value) {  },),
+                Container(
+                  margin: const EdgeInsets.all(25),
+                  child: OutlinedButton(
+                    onPressed: (){
+                      clear();
+                      Navigator.of(context).pushNamed(Home.routeName);
+                    },
+                    child:
+                    const Text("Add FeedBack",
+                      style: TextStyle(fontSize: 20.0)
+                    ),
+                  ),
+                ),
+
+              ],
+            ),
           ),
         ),
       ),
