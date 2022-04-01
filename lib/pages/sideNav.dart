df2b07b81b6353725423784279cfc52195ab7040
duhithu <duhithus@gmail.com>
2022-04-01 09:26:43 +0530

    import healthHabitReminders to sideNav.dart

--- lib/pages/sideNav.dart
+++ lib/pages/sideNav.dart
@@ -1,6 +1,7 @@
 import 'package:ctse_medicine_reminder_app/pages/InjectionReminders.dart';
 import 'package:ctse_medicine_reminder_app/pages/pillReminders.dart';
 import 'package:ctse_medicine_reminder_app/pages/reFillReminder.dart';
+import 'package:ctse_medicine_reminder_app/pages/healthHabitReminders.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
