3b793a437926ed12d668335110ee271cfaf8c841
gavin <gavingunapala06@gmail.com>
2022-04-07 10:19:53 +0530

    home changers done

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,13 +1,14 @@
 import 'package:ctse_medicine_reminder_app/pages/InjectionReminders.dart';
 import 'package:ctse_medicine_reminder_app/pages/pillReminders.dart';
-import 'package:ctse_medicine_reminder_app/pages/healthHabitReminders.dart';
 import 'package:ctse_medicine_reminder_app/pages/reFillReminder.dart';
 import 'package:ctse_medicine_reminder_app/pages/sideNav.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'AddInjectionReminder.dart';
 import 'feedbackForm.dart';
+import 'healthHabitReminders.dart';
 
 class  Home extends StatelessWidget {
   static const String routeName = '/';
@@ -20,78 +21,97 @@ class  Home extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Home"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const SizedBox(width:double.infinity,
-          ),
-
-          Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              width:double.infinity,
+              height: 100,
+            ),
+            Center(child: Text("MedReminder", style: TextStyle( fontSize: 50, fontWeight: FontWeight.bold,
+                foreground: Paint()..style = PaintingStyle.stroke..strokeWidth = 5..color = Colors.blue
+            ))),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                SizedBox(
-                  child: Column(
+
+                const SizedBox(
+                  width:double.infinity,
+                  height: 100,
+                ),
+                const SizedBox(
+
+                ),
+
+                Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      IconButton(
-                        icon: Icon(FontAwesomeIcons.pills, color:Colors.blue),
-                        iconSize: 50,
-                        onPressed: () { Navigator.of(context).pushNamed(PillReminders.routeName);},
+                      SizedBox(
+                        child: Column(
+                          children: [
+                            IconButton(
+                              icon: Icon(FontAwesomeIcons.pills, color:Colors.blue),
+                              iconSize: 50,
+                              onPressed: () { Navigator.of(context).pushNamed(PillReminders.routeName);},
+                            ),
+                            Text("Pill Reminders", style: TextStyle(color: Colors.grey, fontSize: 18, fontWeight: FontWeight.bold)),
+                          ],
+                        ),
                       ),
-                      Text("Pill Reminders", style: TextStyle(color: Colors.grey, fontSize: 18, fontWeight: FontWeight.bold)),
+                      SizedBox(
+                        child: Column(
+                          children: [
+                            IconButton(
+                              icon: Icon(FontAwesomeIcons.prescriptionBottle, color:Colors.blue),
+                              iconSize: 50,
+                              onPressed: () { Navigator.of(context).pushNamed(RefillReminder.routeName);},
+                            ),
+                            Text("Refill Reminders", style: TextStyle(color: Colors.grey, fontSize: 18, fontWeight: FontWeight.bold)),
+                          ],
+                        ),
+
+                      ),
                     ],
                   ),
                 ),
-                SizedBox(
-                  child: Column(
-                    children: [
-                      IconButton(
-                        icon: Icon(FontAwesomeIcons.prescriptionBottle, color:Colors.blue),
-                        iconSize: 50,
-                        onPressed: () { Navigator.of(context).pushNamed(RefillReminder.routeName);},
+                const SizedBox(
+                  height: 60,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      child: Column(
+                        children: [
+                          IconButton(
+                            icon: Icon(FontAwesomeIcons.syringe, color:Colors.blue),
+                            iconSize: 50,
+                            onPressed: () { Navigator.of(context).pushNamed(InjectionReminders.routeName);},
+                          ),
+                          Text("Injection Reminders", style: TextStyle(color: Colors.grey, fontSize: 18, fontWeight: FontWeight.bold)),
+                        ],
                       ),
-                      Text("Refill Reminders", style: TextStyle(color: Colors.grey, fontSize: 18, fontWeight: FontWeight.bold)),
-                    ],
-                  ),
-
+                    ),
+                    SizedBox(
+                      child: Column(
+                        children: [
+                          IconButton(
+                            icon: Icon(FontAwesomeIcons.appleAlt, color:Colors.blue),
+                            iconSize: 50,
+                            onPressed: () {Navigator.of(context).pushNamed(HealthHabitReminders.routeName);},
+                          ),
+                          Text("Health Habit Reminders", style: TextStyle(color: Colors.grey, fontSize: 18, fontWeight: FontWeight.bold)),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ),
-          const SizedBox(
-            height: 60,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              SizedBox(
-                child: Column(
-                  children: [
-                    IconButton(
-                      icon: Icon(FontAwesomeIcons.syringe, color:Colors.blue),
-                      iconSize: 50,
-                      onPressed: () { Navigator.of(context).pushNamed(InjectionReminders.routeName);},
-                    ),
-                    Text("Injection Reminders", style: TextStyle(color: Colors.grey, fontSize: 18, fontWeight: FontWeight.bold)),
-                  ],
-                ),
-              ),
-              SizedBox(
-                child: Column(
-                  children: [
-                    IconButton(
-                      icon: Icon(FontAwesomeIcons.appleAlt, color:Colors.blue),
-                      iconSize: 50,
-                      onPressed: () {Navigator.of(context).pushNamed(HealthHabitReminders.routeName);},
-                    ),
-                    Text("Health Habit Reminders", style: TextStyle(color: Colors.grey, fontSize: 18, fontWeight: FontWeight.bold)),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
